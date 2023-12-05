531ffd4d19eed2f8a6af00ca871d40bf933e6ad9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-05 15:52:12 -0500

    Update chat.js

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import styles from './styles.module.css'
 
 const Chat = ({ className, data, onSubmit }) => {
   const [isMinimized, setIsMinimized] = useState(true)
@@ -33,73 +34,14 @@ const Chat = ({ className, data, onSubmit }) => {
   // add data to state
   
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: isMinimized ? '450px' : '100vh',
-        position: 'absolute',
-        transition: 'all 0.3s ease',
-        width: isMinimized ? '300px' : '100%',
-        bottom: 0,
-        right: 0,
-      }}
-    >
-      <button
-        style={{
-          cursor: 'pointer',
-          backgroundColor: '#3498db',
-          color: '#fff',
-          border: 'none',
-          padding: '5px 10px',
-          borderRadius: '5px',
-        }}
-        onClick={toggleChat}
-      >
-        {isMinimized ? 'Maximize' : 'Minimize'}
-      </button>
-      <div
-        ref={chatBoxRef}
-        style={{
-          flex: 1,
-          overflowY: 'scroll',
-          padding: '10px',
-          boxSizing: 'border-box',
-          border: '1px solid #ccc',
-          marginBottom: '40px',
-        }}
-      >
-        {messages.map((message, index) => (
-          <div key={index}>{message}</div>
-        ))}
+    <div className={styles.chat} style={{ width: isMinimized ? '300px' : '100%', height: isMinimized ? '450px' : '100%' }}>
+      <button type="button" onClick={toggleChat}>{isMinimized ? 'Maximize' : 'Minimize'}</button>
+      <div className={styles.messages} ref={chatBoxRef}>
+        {messages.map((message, index) => <div key={index}>{message}</div>)}
       </div>
-      <form
-        onSubmit={handleOnSubmit}
-        style={{
-          display: 'flex',
-          padding: '10px',
-          boxSizing: 'border-box',
-          backgroundColor: '#f0f0f0',
-          position: 'absolute',
-          bottom: 0,
-          width: '100%',
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Type your message..."
-          value={inputValue}
-          onChange={handleOnChange}
-          style={{
-            boxSizing: 'border-box',
-            marginRight: '10px',
-            transition: 'all 0.3s ease',
-            width: '100%',
-          }}
-        />
-        <button type="submit" style={{ cursor: 'pointer' }}>
-          Send
-        </button>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" placeholder="Type your message..." value={inputValue} onChange={handleOnChange} />
+        <button type="submit">Send</button>
       </form>
     </div>
   )
